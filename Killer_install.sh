659a687aa0e95ed8a8906413e221d152d3e06ea7
Extremmer <68739346+Extremmer@users.noreply.github.com>
2021-02-10 18:24:04 +0300

    Delete Killer_install.sh

--- Killer_install.sh
+++
@@ -1,7 +0,0 @@
-#===================================
-#!/bin/bash #----------------------|
-sudo apt autoremove -y #-----------|
-sudo apt install python -y #-------|
-sudo apt install python3-pip -y #--|
-pip install termcolor #------------|
-#===================================
