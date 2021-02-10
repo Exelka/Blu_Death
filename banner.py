53072ddf379c4f259cfe85bc9af77d0d2365eed7
Extremmer <68739346+Extremmer@users.noreply.github.com>
2021-02-10 18:24:19 +0300

    Delete banner.py

--- banner.py
+++
@@ -1,6 +0,0 @@
-from termcolor import colored
-print(colored('mmmmm  m      m    m        m    m mmmmm  m      m      mmmmmm mmmmm', 'red'))
-print(colored('#    # #      #    #        #  m"    #    #      #      #      #   "#', 'red'))
-print(colored('#mmmm" #      #    #        #m#      #    #      #      #mmmmm #mmmm"', 'red'))
-print(colored('#    # #      #    #        #  #m    #    #      #      #      #   "m', 'red'))
-print(colored('#mmmm" #mmmmm "mmmm"        #   "m mm#mm  #mmmmm #mmmmm #mmmmm #    m', 'red'))
