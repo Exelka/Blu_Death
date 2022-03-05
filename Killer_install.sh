a2b709a90f8582c8cc7457541728cd8b877355c5
Exelka <68739346+Exelka@users.noreply.github.com>
2022-03-06 01:54:53 +0300

    Update Killer_install.sh

--- Killer_install.sh
+++ Killer_install.sh
@@ -1,6 +1,7 @@
-#===================================
-#!/bin/bash #----------------------|
-sudo apt install python -y #-------|
-sudo apt install python3-pip -y #--|
-pip install termcolor #------------|
-#===================================
+#===========================================================
+#!/bin/bash #----------------------------------------------|
+sudo apt install git||apt install git #--------------------|
+sudo apt install python -y||apt install python -y| #-------|
+sudo apt install python3-pip -y||apt install python3-pip#--|
+pip install termcolor #------------------------------------|
+#===========================================================
